17ef9d7d5393e5290d72477245ccedcee5ec1699
Flávio Jr <fgcj@outlook.com>
2019-09-13 21:16:59 -0300

    Projeto 2

---
+++ s05e01/ContaCorrente.java
@@ -0,0 +1,11 @@
+
+public class ContaCorrente extends Conta {
+
+	public ContaCorrente(double saldo, double limite, double juros) {
+		super(saldo, limite, juros);
+		
+	}
+	
+	
+
+}
---
+++ s05e01/Principal.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Cliente> clientesArray = new ArrayList<Cliente>();
+	
+		
+	//------------------------------------------------------------------------------------
+		Conta cc1 = new ContaCorrente(500, 1000, 00);
+		Conta cc2 = new ContaCorrente(500, 1000, 00);
+
+		Cliente c = new Cliente("flavio", "000.000.111", cc1);
+		clientesArray.add(c);
+		Cliente c2 = new Cliente("maria", "000.222.111", cc2);
+		clientesArray.add(c2);
+		
+		cc1.depositar(30);
+		cc2.sacar(170);
+		
+		
+		cc1.transferir(clientesArray,100, "000.222.111");
+		
+	//------------------------------------------------------------------------------------	
+
+	}
+
+}
