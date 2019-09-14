16d12e7b77479e08912eb961c8534ad818d2b5aa
Flávio Jr <fgcj@outlook.com>
2019-09-13 21:05:17 -0300

    Exerc. Modificadores de acess.+ met. static

--- s05e00/src/com/company/Main.java
+++ s05e00/src/com/company/Main.java
@@ -1,31 +1,18 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-	    Moto moto = new Moto();
-	    System.out.println("init_A_B, stop, abastecer, viajar_A, embarcar_P, desembarcar_P");
+        Conta conta = new Conta();
+        conta.setSaldo(400);
+        conta.setTitular("Pedro");
+        conta.saca(200);
+        Conta c = new Conta ();
+        c.setTitular("zezãoDasTapiocas");
+        c.status();
 
 
-        while (true) {
-            String line = scan.nextLine();
-            String [] ui = line.split (" ");
-            if (ui[0].equals("stop")) {
-                break;
-            } else if (ui[0].equals("init")) {
-                moto = new Moto();
-            } else if (ui[0].equals("abastecer")) {
-                moto.abastecer(Integer.parseInt(ui[1]));
-            } else if (ui[0].equals("viajar")) {
-                moto.viajar(Integer.parseInt(ui[1]));
-            } else if (ui[0].equals("embarcar")) {
-                moto.embarcar(Integer.parseInt(ui[1]), ui[2]);
-            }
-               System.out.println (moto.embarcar(ui[1], Integer.parseInt(ui[2]));
 
 
-            }
+    }
 }
